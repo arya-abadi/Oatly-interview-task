91ddfe763e3faca17d62dc83d076724595289071
hamidreza8968 <hamidreza.seydabadi68@gmail.com>
2025-05-07 08:46:05 +0200

    feat: create Badge component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,20 @@ import nordicSeedImage from "../../public/assets/images/NordGen.webp";
 
 export default function Home() {
     return (
-        <main className="container mx-auto my-6 relative">
-            {/*<h1 className="text-heading font-heading font-bold tracking-tight absolute left-0">{data.title}</h1>*/}
-
-            <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 p-6">
-                <div className="flex flex-col gap-0 lg:gap-2">
-                {data.paragraphs.slice(0, 2).map((text, idx) => (
-                    <p key={idx} className="bg-gray-100 p-4 rounded shadow">{text}</p>
-                ))}
+        <main className="container mx-auto py-12">
+            <div className="grid grid-cols-3 gap-4">
+                <div className="flex flex-col gap-6">
+                    <h1 className="text-heading font-heading tracking-tighter z-10 uppercase w-[96rem]">{data.title}</h1>
+                    <div className="flex items-center gap-2">
+                        <div className="border border-black rounded-full py-1 px-4 w-fit">By {data?.author} . {data?.category}</div>
+                        <div className="border border-black rounded-full py-1 px-4 w-fit">{data?.publishedAt}</div>
+                    </div>
+                    <p className="text-prelude font-prelude w-[40rem] z-10">{data.prelude}</p>
+                    {data.paragraphs.slice(0, 2).map((text, idx) => (
+                        <p key={idx} className="bg-gray-100 p-4 rounded shadow">{text}</p>
+                    ))}
                 </div>
-                <div className="lg:col-span-2 relative rounded shadow">
+                <div className="col-span-2 relative rounded shadow">
                     <Image
                         src={nordicSeedImage}
                         alt="Nordic Seeds"
---
+++ src/components/Badge/Badge.tsx
@@ -0,0 +1,9 @@
+import {BadgeProps} from '@/types/IBadge';
+
+export default function Badge({children, className = ""}: BadgeProps) {
+    return (
+        <div className={`border border-black rounded-full py-1 px-4 w-fit ${className}`}>
+            {children}
+        </div>
+    );
+}
